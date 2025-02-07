686ae1106ad009c97126aed9b79a89c41bcb0186
Darren <nardol.darren@gmail.com>
2025-02-07 14:04:35 +0100

    sort by rôle tags

--- src/components/characters.jsx
+++ src/components/characters.jsx
@@ -19,8 +19,12 @@ function Characters() {
 
         const json = await response.json();
         const data = json.data;
-        const champName = Object.keys(data);
+        const champName = Object.values(data).map((champ) => ({
+          id: champ.id,
+          tags: champ.tags,
+        }));
         setChampions(champName);
+        console.log(champName);
 
         console.log(champName);
       } catch (error) {
@@ -54,15 +58,113 @@ function Characters() {
 
   return (
     <>
-      {champions.map((names, index) => (
-        <button key={index}>
-          <p>{names}</p>
-          <img
-            src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${names}.png`}
-            alt="portrait du champion"
-          />
-        </button>
-      ))}
+      <div className="champion_assassin">
+        <h2>Assassin</h2>
+
+        {champions.map((champ, index) => {
+          if (champ.tags.includes("Assassin")) {
+            return (
+              <button key={index}>
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champ.id}.png`}
+                  alt="portrait du champion"
+                />
+                <p>{champ.id}</p>
+              </button>
+            );
+          }
+        })}
+      </div>
+
+      <div className="champion_fighter">
+        <h2>Fighter</h2>
+
+        {champions.map((champ, index) => {
+          if (champ.tags.includes("Fighter")) {
+            return (
+              <button key={index}>
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champ.id}.png`}
+                  alt="portrait du champion"
+                />
+                <p>{champ.id}</p>
+              </button>
+            );
+          }
+        })}
+      </div>
+
+      <div className="champion_mage">
+        <h2>Mage</h2>
+
+        {champions.map((champ, index) => {
+          if (champ.tags.includes("Mage")) {
+            return (
+              <button key={index}>
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champ.id}.png`}
+                  alt="portrait du champion"
+                />
+                <p>{champ.id}</p>
+              </button>
+            );
+          }
+        })}
+      </div>
+
+      <div className="champion_marksman">
+        <h2>Marskman</h2>
+
+        {champions.map((champ, index) => {
+          if (champ.tags.includes("Marksman")) {
+            return (
+              <button key={index}>
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champ.id}.png`}
+                  alt="portrait du champion"
+                />
+                <p>{champ.id}</p>
+              </button>
+            );
+          }
+        })}
+      </div>
+
+      <div className="champion_support">
+        <h2>Support</h2>
+
+        {champions.map((champ, index) => {
+          if (champ.tags.includes("Support")) {
+            return (
+              <button key={index}>
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champ.id}.png`}
+                  alt="portrait du champion"
+                />
+                <p>{champ.id}</p>
+              </button>
+            );
+          }
+        })}
+      </div>
+
+      <div className="champion_tank">
+        <h2>Tank</h2>
+
+        {champions.map((champ, index) => {
+          if (champ.tags.includes("Tank")) {
+            return (
+              <button key={index}>
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champ.id}.png`}
+                  alt="portrait du champion"
+                />
+                <p>{champ.id}</p>
+              </button>
+            );
+          }
+        })}
+      </div>
     </>
   );
 }
